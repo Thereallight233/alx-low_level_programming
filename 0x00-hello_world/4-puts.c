4f3c998cb3ef94de4a3423a5a09c4f77bedcc689
Jonathan Lartey <larteycorp14@gmail.com>
2023-07-15 15:24:20 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - Entry point 
+ * main - print a line of code using puts
  *
- * Return: Always 0(Success)
+ * Return: Always 0
  */
 int main(void)
 {
-   puts("Programming is like building a multilingual puzzle");
-   return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
