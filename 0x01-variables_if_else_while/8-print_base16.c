d83bbf8ae1ea4c1125d52d9659afd80aa66ad7c1
Jonathan Lartey <larteycorp14@gmail.com>
2023-07-17 20:06:30 +0000

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,7 +9,7 @@ int main(void)
 	int n;
 	int m;
 
-	for (n = 48; n <= 58; n++)
+	for (n = 48; n <= 57; n++)
 	{
 		putchar(n);
 	}
@@ -17,6 +17,6 @@ int main(void)
 	{
 		putchar(m);
 	}
-		putchar('\n');
+	putchar('\n');
 	return (0);
 }
