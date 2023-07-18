c431902254b87aa80a4cd2ce003621579faac1d9
Jonathan Lartey <larteycorp14@gmail.com>
2023-07-18 19:44:20 +0000

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - Entry point
- * Description: 'prints _putchar to stdout'
- * Return: Always 0
+ *main - Entry point
+ *Description: 'print _putchar'
+ *Return: Always 0
  */
 int main(void)
 {
--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,9 @@
-#include <stdio.h>
+#include "main.h"
+
 /**
- * main - Entry point
- * Description: Prints the alphabets in lowercase
- * Return: Always 0
+ *
  */
-int main(void)
+void print_alphabet(void)
 {
-	int i;
-
-	for (i = 97; i <= 122; i++)
-	{
-		putchar(i);
-	}
-	putchar('\n');
-
-	return (0);
+	
 }
