0d716fc427143cfa3d70687148ad3975b9004866
Jonathan Lartey <larteycorp14@gmail.com>
2023-07-18 15:34:45 +0000

    0-puchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Description: 'prints _putchar to stdout'
+ * Return: Always 0
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
