379aa97dbe2c43d06c4d0a93118b8df6f265dba6
Jonathan Lartey <larteycorp14@gmail.com>
2023-07-18 16:47:25 +0000

    tasks

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,29 +1,33 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
- *
- * Return: Always 0
- */
+ *  * main - prints all possible different combinations of three digits
+ *   * Return: Always 0 (Success)
+ *    */
 int main(void)
 {
-	int a;
-	int b;
-	int c;
+		int n, m, l;
 
-	for (a = 48; a <= 57; a++)
-	{
-		putchar(a);
-	}
-	for (b = a + 1; b <= 9; b++)
-	{
-		putchar(b);
-	}
-	for (c = b + 1; c <= 9; c++)
-	{
-		putchar(c);
-		putchar(',');
-		putchar(' ');
-	}
-	putchar('\n');
-	return (0);
+			for (n = 48; n < 58; n++)
+					{
+								for (m = 49; m < 58; m++)
+											{
+															for (l = 50; l < 58; l++)
+																			{
+																								if (l > m && m > n)
+																													{
+																																			putchar(n);
+																																								putchar(m);
+																																													putchar(l);
+																																																		if (n != 55 || m != 56)
+																																																								{
+																																																															putchar(',');
+																																																																					putchar(' ');
+																																																																										}
+																																																						}
+																											}
+																	}
+									}
+				putchar('\n');
+					return (0);
 }
