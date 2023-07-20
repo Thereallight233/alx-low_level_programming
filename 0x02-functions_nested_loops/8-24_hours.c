f5d3d1f07644449977ea6a0fd2fead755bfa81ee
Jonathan Lartey <larteycorp14@gmail.com>
2023-07-20 01:00:31 +0000

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,16 +10,19 @@ void jack_bauer(void)
 	int i, j;
 	
 	i = 0;
-	while (i < 24); i++;
+	while (i < 24)
 	{
 		j = 0;
-		while (j < 60); j++;
+		while (j < 60)
 		{
 			_putchar((i / 10) + '0');
 			_putchar((i % 10) + '0');
 			_putchar(':');
 			_putchar((j / 10) + '0');
+			_putchar('\n');
+			j++;
 		}
 	}
-	_putchar('\n');
+	i++;
+
 }
