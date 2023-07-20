f3fd9d1d2dbd23788f1c77f41803779e93389683
Jonathan Lartey <larteycorp14@gmail.com>
2023-07-20 00:18:04 +0000

    task

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,19 @@
 #include "main.h"
 
 /**
- *
+ *main - Entry point
+ *Description: Prints the alphabets in lowercase
+ *Return: Always 0
  */
+char c;
+
 void print_alphabet(void)
 {
-	
+	for(c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}	
+	putchar('\n');
+
+	return (0);
 }
