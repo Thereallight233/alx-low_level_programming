5919ff12ecf192ed3a72f0cd8bc01dca99d2b7f7
Jonathan Lartey <larteycorp14@gmail.com>
2023-07-20 07:44:37 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9.
+ * followed by a new line.
+ * @c: ASCII character.
+ */
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 48; c < 57; c++)
+	{
+		_putchar(c + '0');
+	}
+	_putchar('\n');
+}
