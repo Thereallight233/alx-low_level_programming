8936b1a52b04c4b65898bea7849a774c885b7bc7
Jonathan Lartey <larteycorp14@gmail.com>
2023-09-12 20:45:42 +0000

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,10 +1,11 @@
-#ifndef main.h
-#define main.h
+#ifndef main_h
+#define main_h
 
+void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
-int print_sign(int n);
+int print_sigin(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
