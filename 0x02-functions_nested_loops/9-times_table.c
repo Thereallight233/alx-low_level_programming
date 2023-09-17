c23b8720f93c1003136dd0a58a3a91b7b869d36d
Jonathan Lartey <larteycorp14@gmail.com>
2023-09-17 20:53:04 +0000

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,26 +12,21 @@ void times_table(void)
 		for (j = 0; j < 10; j++)
 		{
 			k = i * j;
-			if (j == 0)
+			if (j != 0)
 			{
-				_putchar(k + '0');
+				_putchar(',');
+				_putchar(' ');
+				if (k < 10)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar((k / 10) + '0');
+				}
 			}
-		}
-		if (k < 10 && j != 0)
-		{
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
-			_putchar(k + '0');
-		}
-		else if (k >= 10)
-		{
-			_putchar(',');
-			_putchar(' ');
-			_putchar((k / 10) + '0');
 			_putchar((k % 10) + '0');
 		}
 		_putchar('\n');
 	}
 }
-
